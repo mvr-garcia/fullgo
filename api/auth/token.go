669b7aa403ed282f90737315caaa60f8c63f758e
Marcos Garcia <mvrgarcia05@gmail.com>
2022-10-30 02:00:26 -0300

    auth package

--- api/auth/token.go
+++ api/auth/token.go
@@ -1,1 +1,99 @@
 package auth
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+
+	jwt "github.com/dgrijalva/jwt-go"
+)
+
+// Pretty display the claims licely in the terminal
+func Pretty(data interface{}) {
+
+	res, err := json.Marshal(data)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	fmt.Println(string(res))
+}
+
+func CreateToken(userId uint32) (string, error) {
+
+	claims := jwt.MapClaims{}
+	claims["authorized"] = true
+	claims["user_id"] = userId
+	claims["exp"] = time.Now().Add(time.Hour * 1).Unix() // Token expires after 1 hour
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString([]byte(os.Getenv("API_SECRET")))
+}
+
+func ExtractToken(r *http.Request) string {
+
+	keys := r.URL.Query()
+	token := keys.Get("token")
+	if token != "" {
+		return token
+	}
+
+	bearerToken := r.Header.Get("Authorization")
+	if len(strings.Split(bearerToken, " ")) == 2 {
+		return strings.Split(bearerToken, " ")[1]
+	}
+
+	return ""
+}
+
+func TokenParser(tokenString string) (*jwt.Token, error) {
+	token, err := jwt.Parse(
+		tokenString,
+		func(t *jwt.Token) (interface{}, error) {
+			if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, fmt.Errorf("unexpected signing method: %v", t.Header["alg"])
+			}
+			return []byte(os.Getenv("API_SECRET")), nil
+		},
+	)
+	return token, err
+}
+
+func ValidateToken(r *http.Request) error {
+
+	tokenString := ExtractToken(r)
+	token, err := TokenParser(tokenString)
+	if err != nil {
+		return err
+	}
+
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		Pretty(claims)
+	}
+	return nil
+}
+
+func ExtractTokenID(r *http.Request) (uint32, error) {
+
+	tokenString := ExtractToken(r)
+	token, err := TokenParser(tokenString)
+	if err != nil {
+		return 0, err
+	}
+
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		uid, err := strconv.ParseUint(fmt.Sprintf("%.0f", claims["user_id"]), 10, 32)
+		if err != nil {
+			return 0, err
+		}
+		return uint32(uid), nil
+	}
+
+	return 0, nil
+}
